0ba5ac03210ebd985f1907abd108463cd3011021
eyal@tan-doo.com <eyal@tan-doo.com>
2023-05-11 13:26:59 +0000

    8

--- dbt_course_project/models/example/my_test_model.sql
+++ dbt_course_project/models/example/my_test_model.sql
@@ -1,7 +1,7 @@
 -- my_test_model.sql
 
 -- Include the Jinja templating syntax to execute the Python script
-{% set test_result = run_python_script('my_test_script.perform_test') %}
+{% set test_result = run_python_script('macros.my_test_script.perform_test') %}
 
 -- Fail the test if the result is False
 {% if not test_result %}
