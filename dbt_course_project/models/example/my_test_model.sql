9b9a54e975a4877d02f314d6058521cbbde37cbd
eyal@tan-doo.com <eyal@tan-doo.com>
2023-05-11 14:20:59 +0000

    40

--- dbt_course_project/models/example/my_test_model.sql
+++ dbt_course_project/models/example/my_test_model.sql
@@ -1,8 +1,8 @@
 -- my_test_model.sql
 
-{% set test_result_sql = """SELECT {{ test_script.perform_test() }}""" %}
-{% set test_result = run_query(test_result_sql) %}
+{{ dbt_utils.run_python_script("macros.my_test_script.perform_test") }}
+{% set test_result = return %}
 
-{% if test_result[0][0] != 1 %}
+{% if not test_result %}
   {% do log('Test failed!', 'error') %}
 {% endif %}
