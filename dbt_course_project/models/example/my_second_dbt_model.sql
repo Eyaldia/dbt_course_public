a7f433551e94abda83ba48f5f2c0572d68801959
Eyaldia <105989692+Eyaldia@users.noreply.github.com>
2023-05-14 11:55:05 +0300

    Update my_second_dbt_model.sql
    8

--- dbt_course_project/models/example/my_second_dbt_model.sql
+++ dbt_course_project/models/example/my_second_dbt_model.sql
@@ -1,6 +1,4 @@
-
--- Use the `ref` function to select from other models
+{{ config(materialized='table',alias='ORDERS_T',DATABASE='MAIN_DB',SCHEMA='MAIN') }}
 
 select *
-from {{ ref('my_first_dbt_model') }}
-where id = 1
+from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."ORDERS" 
