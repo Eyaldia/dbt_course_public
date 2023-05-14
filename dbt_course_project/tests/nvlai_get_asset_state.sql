1bc1c5d08092c221e79c628ff1b4fc669b120a02
eyal@tan-doo.com <eyal@tan-doo.com>
2023-05-14 09:18:01 +0000

    15

--- dbt_course_project/tests/nvlai_get_asset_state.sql
+++ dbt_course_project/tests/nvlai_get_asset_state.sql
@@ -1,2 +1,2 @@
-    select  NVLAI.PUBLIC.NVL_AI_GET_ASSET_STATE('1683120204323x323537520054763500', {{ref('snowflake_cusomer_info')}}) ASSET_STATUS
+    select  NVLAI.PUBLIC.NVL_AI_GET_ASSET_STATE('1683120204323x323537520054763500', '{{ref('snowflake_cusomer_info')}}') ASSET_STATUS
     where ASSET_STATUS not like '%Verified%'
